a07b69e2cbc582ba18957f98dfd6d81cc80eaa0b
Allan Ramsay <a.ramsay@gmail.com>
2017-03-13 00:18:12 +1300

    Add volumes

--- drunner/service.lua
+++ drunner/service.lua
@@ -2,6 +2,7 @@
 
 -- add any configuration variables needed with:
 -- addconfig( VARIABLENAME, DEFAULTVALUE, DESCRIPTION )
+addconfig("MYSQL_ROOT_PASSWORD", "symphony", "Root password for MySQL")
 
 function uninstall()
   stop()
@@ -14,14 +15,26 @@ end
 
 function install()
   dockerpull("mysql:5.6")
+  dockercreatevolume("${SERVICENAME}-mysql")
+  dockercreatevolume("${SERVICENAME}-web")
 -- e.g. pull relevant containers with dockerpull, create volumes with dockercreatevolume
 end
 
 function backup()
+  docker("pause", "${SERVICENAME}")
+  docker("pause", "${SERVICENAME}-mysql")
+
+  dockerbackup("${SERVICENAME}-web")
+  dockerbackup("${SERVICENAME}-mysql")
+
+  docker("unpause", "${SERVICENAME}")
+  docker("unpause", "${SERVICENAME}-mysql")
 -- pause containers with docker("pause",containername), backup volumes with dockerbackup(volumename), then unpause
 end
 
 function restore()
+  dockerrestore("${SERVICENAME}-web")
+  dockerrestore("${SERVICENAME}-mysql")
 -- restore volumes with dockerrestore(volumename)
 end
 
@@ -32,7 +45,11 @@ end
 function start()
   start_mysql()
   -- dieunless( dockerwait("${SERVICENAME}-mysql", "3306"), "MySQL did not respond.")
-  result,output=docker("run", "-d", "--name", "${SERVICENAME}", "-p", "80:80", "--link", "${SERVICENAME}-mysql:mysql", "symphony")
+  result,output=docker("run", "-d", "--name", "${SERVICENAME}",
+    "-p", "80:80", 
+    "--link", "${SERVICENAME}-mysql:mysql", 
+    "-v", "${SERVICENAME}-web:/var/www/html",
+    "symphony")
 end
 
 function stop()
@@ -44,6 +61,7 @@ function start_mysql()
   result,output=docker("run", "-d", "--name", "${SERVICENAME}-mysql",
     "-e", "MYSQL_ROOT_PASSWORD=symphony",
     "-e", "MYSQL_DATABASE=symphony",
+    "-v", "${SERVICENAME}-mysql:/var/lib/mysql",
     "mysql:5.6")
 end
 
